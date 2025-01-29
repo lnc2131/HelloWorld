7cbd19fdd53dc1ae4055bbf26572e2e62a1b7f5e
lnc2131 <lnc2131@bu.edu>
2025-01-29 16:14:00 +0000

    Added button to toggle Hello World text

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.foundation.layout.width
 import com.example.helloworldapp.ui.theme.HelloWorldAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HelloWorldScreen()
                 }
             }
         }
@@ -31,17 +35,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name",
-        modifier = modifier
-    )
+fun HelloWorldScreen() {
+    var textVisible by remember{mutableStateOf(false)}
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {textVisible = !textVisible}) {
+            Text(text = if (textVisible) "Hide Text" else "Show Text")
+        }
+        if (textVisible) {
+            Text(
+                text = "Hello World",
+                modifier = Modifier.padding(top=16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldAppTheme {
-        Greeting("Lucas")
+        HelloWorldScreen()
     }
 }
